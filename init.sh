9ce18737af104ba906dbf3ec74869f8ffb8e0d14
Sumit Chohan <sumitchohan@Sumits-MacBook-Pro.local>
2018-05-28 11:21:02 -0500

    df

---
+++ init.sh
@@ -0,0 +1,5 @@
+unzip protoc-3.2.0-linux-x86_64.zip -d protoc3
+mv protoc3/bin/* /usr/local/bin/
+mv protoc3/include/* /usr/local/include/
+curl -OL https://pythonprogramming.net/static/downloads/machine-learning-data/object-detection-macaroni.zip
+unzip object-detection-macaroni.zip
