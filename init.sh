913e1027d9afa366ba705bd773aa63028110cd0a
Sumit Chohan <sumitchohan@Sumits-MacBook-Pro.local>
2018-05-28 12:01:54 -0500

    df

--- init.sh
+++ init.sh
@@ -1,4 +1,4 @@
-pip install --upgrade tensorflow
+pip3 install --upgrade tensorflow
 
 curl -OL https://github.com/google/protobuf/releases/download/v3.2.0/protoc-3.2.0-linux-x86_64.zip
 unzip protoc-3.2.0-linux-x86_64.zip -d protoc3
