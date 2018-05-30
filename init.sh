49ed05c700e460f378e7e3eba479ed97c50d22eb
Sumit Chohan <sumitchohan@Sumits-MacBook-Pro.local>
2018-05-29 19:46:32 -0500

    df

--- init.sh
+++ init.sh
@@ -7,5 +7,10 @@ mv protoc3/include/* /usr/local/include/
 curl -OL https://pythonprogramming.net/static/downloads/machine-learning-data/object-detection-macaroni.zip
 unzip object-detection-macaroni.zip
 cp -R models/research/object_detection/. object_detection
+python3 ./xml_to_csv.py
+python3 ./generate_tfrecord.py
 protoc object_detection/protos/*.proto --python_out=.
 export PYTHONPATH=$PYTHONPATH:`pwd`:`pwd`/slim
+curl -OL http://download.tensorflow.org/models/object_detection/ssd_mobilenet_v1_coco_11_06_2017.tar.gz
+tar -xzf ssd_mobilenet_v1_coco_11_06_2017.tar.gz
+cp object_detection/train.py ./train.py
